fdb6f8d29a088d7af3753a69db212853eb4f73ff
Dion <dion.boles@gmail.com>
2019-11-18 12:24:11 -0500

    test

--- src/calc.js
+++ src/calc.js
@@ -4,18 +4,22 @@ class Calc extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            salary : 0
+            salary : 0,
+            week : 0
         }
     }
     data(event){
         let text = event.target.value
         text.indexOf(',') != -1 ? text =  parseFloat(text.split(",").join("")) : text = text
+        let week = text/52
         console.log()
         
         text = text /52
         text = text/40
+      
         this.setState({
-            salary : Math.round(text)
+            salary : Math.round(text).toFixed(2),
+            week : Math.round(week).toFixed(2)
         })
     }
     render(){
@@ -23,6 +27,7 @@ class Calc extends React.Component{
             <div>
                 <h1 className="title">Salary to Hourly Rate </h1>
                 <h2 className="subtitle">Your Hourly Rate :  {this.state.salary}</h2>
+                <h2 className="subtitle">Your Weekly Rate :  {this.state.week}</h2>
                 <div>
                     <label>Enter Yearly Salary  :</label>
                     <input className="inputSalary" type="text"onChange={this.data.bind(this)}></input>
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     ],
     output:{
       path: __dirname + "/dist",
-      publicPath: '/',
+      publicPath: './',
       filename: 'bundle.js'
     }
 }  
